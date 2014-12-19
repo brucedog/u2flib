45dbead5f3e4f968934a248814d82b137412f6ee
bryce foster <bryce1337@gmail.com>
2014-12-19 16:22:11 -0600

    Added unit tests for utils class
    added two unit tests to Utils class for formatting to and from base64

---
+++ UnitTests/U2F/UtilsUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using u2flib.Util;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UtilsUnitTests
+    {
+        [TestMethod]
+        public void ByteArrayToBase64String_Test()
+        {
+            byte[] testStringByteArray = Utils.GetBytes(TestConts.SERVER_CHALLENGE_REGISTER_BASE64);
+
+            string result = Utils.ByteArrayToBase64String(testStringByteArray);
+
+            Assert.IsFalse(result.Contains("+"));
+            Assert.IsFalse(result.Contains("/"));
+            Assert.IsFalse(result.Contains("="));
+        }
+
+        [TestMethod]
+        public void Base64StringToByteArray_Test()
+        {
+            byte[] result = Utils.Base64StringToByteArray(TestConts.SERVER_CHALLENGE_REGISTER_BASE64);
+
+            Assert.IsTrue(result.Length > 0);
+        }
+    }
+}
