5d7b3e36980c6b6cbcc591955488edfd86083b24
bryce foster <bryce1337@gmail.com>
2015-02-05 16:13:23 -0600

    Added js lib for browser detection
    Added lib for browser detection to prompt user if they arent using
    chrome or have the extension.

--- DemoU2FSite/App_Start/BundleConfig.cs
+++ DemoU2FSite/App_Start/BundleConfig.cs
@@ -19,21 +19,9 @@ namespace DemoU2FSite
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
-            
-//            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-//                        "~/Content/themes/base/jquery.ui.core.css",
-//                        "~/Content/themes/base/jquery.ui.resizable.css",
-//                        "~/Content/themes/base/jquery.ui.selectable.css",
-//                        "~/Content/themes/base/jquery.ui.accordion.css",
-//                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-//                        "~/Content/themes/base/jquery.ui.button.css",
-//                        "~/Content/themes/base/jquery.ui.dialog.css",
-//                        "~/Content/themes/base/jquery.ui.slider.css",
-//                        "~/Content/themes/base/jquery.ui.tabs.css",
-//                        "~/Content/themes/base/jquery.ui.datepicker.css",
-//                        "~/Content/themes/base/jquery.ui.progressbar.css",
-//                        "~/Content/themes/base/jquery.ui.theme.css"));
 
+            bundles.Add(new ScriptBundle("~/bundles/browser").Include(
+                        "~/Scripts/browser.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.min.css"));
         }
