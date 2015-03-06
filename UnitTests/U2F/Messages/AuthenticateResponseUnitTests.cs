3a94f8b61a0f0716561f8f46240d99697248fd7d
bryce foster <bryce1337@gmail.com>
2015-03-06 07:48:02 -0600

    Fixed Unit tests
    Fixed failing unit tests

--- UnitTests/U2F/Messages/AuthenticateResponseUnitTests.cs
+++ UnitTests/U2F/Messages/AuthenticateResponseUnitTests.cs
@@ -6,7 +6,7 @@ namespace UnitTests.Messages
     [TestClass]
     public class AuthenticateResponseUnitTests
     {
-        private const string JsonData = "{\"SignatureData\":\"AQAAAAEwRAIgS18M0XU0zt2MNO4JVw71QqNT30Q2AwzkPUBt6HC4R3gCICZ7uZj6ybcmbrYOfLC16r39W6lhT1PHsiJy7BAEepI/\",\"ClientData\":" + "\"eyJ0eXAiOiJuYXZpZ2F0b3IuaWQuZ2V0QXNzZXJ0aW9uIiwiY2hhbGxlbmdlIjoib3BzWHFVaWZEcmlBQW1XY2xpbmZiUzBlLVVTWTBDZ3lKSGVfT3RkN3o4byIsImNpZF9wdWJrZXkiOnsia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsIngiOiJIelF3bGZYWDdRNFM1TXRDQ25aVU5CdzNSTXpQTzl0T3lXakJxUmw0dEo4IiwieSI6IlhWZ3VHRkxJWngxZlhnM3dOcWZkYm43NWhpNC1fNy1CeGhNbGp3NDJIdDQifSwib3JpZ2luIjoiaHR0cDovL2V4YW1wbGUuY29tIn0=\"," + "\"KeyHandle\":\"KlUt/bdHftZf2EEz+GGWAQsiFbV9p10xW3uej+LjklpgGVUbq2HRZZFlnLrwC0lQ96v+ZmDi4Ab3aGi3ctcMJQ==\"}";
+        private const string JsonData = "{\"SignatureData\":\"AQAAAAEwRAIgS18M0XU0zt2MNO4JVw71QqNT30Q2AwzkPUBt6HC4R3gCICZ7uZj6ybcmbrYOfLC16r39W6lhT1PHsiJy7BAEepI_\",\"ClientData\":" + "\"eyJ0eXAiOiJuYXZpZ2F0b3IuaWQuZ2V0QXNzZXJ0aW9uIiwiY2hhbGxlbmdlIjoib3BzWHFVaWZEcmlBQW1XY2xpbmZiUzBlLVVTWTBDZ3lKSGVfT3RkN3o4byIsImNpZF9wdWJrZXkiOnsia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsIngiOiJIelF3bGZYWDdRNFM1TXRDQ25aVU5CdzNSTXpQTzl0T3lXakJxUmw0dEo4IiwieSI6IlhWZ3VHRkxJWngxZlhnM3dOcWZkYm43NWhpNC1fNy1CeGhNbGp3NDJIdDQifSwib3JpZ2luIjoiaHR0cDovL2V4YW1wbGUuY29tIn0\"," + "\"KeyHandle\":\"KlUt_bdHftZf2EEz-GGWAQsiFbV9p10xW3uej-LjklpgGVUbq2HRZZFlnLrwC0lQ96v-ZmDi4Ab3aGi3ctcMJQ\"}";
 
         [TestMethod]
         public void AuthenticateResponse_ConstructsProperly()
--- UnitTests/U2F/Messages/StartedAuthenticationUnitTests.cs
+++ UnitTests/U2F/Messages/StartedAuthenticationUnitTests.cs
@@ -6,14 +6,15 @@ namespace UnitTests.Messages
     [TestClass]
     public class StartedAuthenticationUnitTests
     {
-        private const string JsonData = "{\"Version\":\"U2F_V2\",\"KeyHandle\":\"KlUt/bdHftZf2EEz+GGWAQsiFbV9p10xW3uej+LjklpgGVUbq2HRZZFlnLrwC0lQ96v+ZmDi4Ab3aGi3ctcMJQ==\",\"Challenge\":\"opsXqUifDriAAmWclinfbS0e-USY0CgyJHe_Otd7z8o\",\"AppId\":\"http://example.com\"}";
+        private const string JsonData = "{\"Version\":\"U2F_V2\",\"KeyHandle\":\"KlUt_bdHftZf2EEz-GGWAQsiFbV9p10xW3uej-LjklpgGVUbq2HRZZFlnLrwC0lQ96v-ZmDi4Ab3aGi3ctcMJQ\",\"Challenge\":\"opsXqUifDriAAmWclinfbS0e-USY0CgyJHe_Otd7z8o\",\"AppId\":\"http://example.com\"}";
 
         [TestMethod]
         public void StartedAuthentication_ConstructsProperly()
         {
-            StartedAuthentication startedAuthentication =
-                new StartedAuthentication(TestConts.SERVER_CHALLENGE_SIGN_BASE64, TestConts.APP_ID_ENROLL,
-                                          TestConts.KEY_HANDLE_BASE64);
+            StartedAuthentication startedAuthentication = new StartedAuthentication(
+                    TestConts.SERVER_CHALLENGE_SIGN_BASE64, 
+                    TestConts.APP_ID_ENROLL,
+                    TestConts.KEY_HANDLE_BASE64);
 
             Assert.IsNotNull(startedAuthentication);
             Assert.IsNotNull(startedAuthentication.Version);
@@ -41,9 +42,10 @@ namespace UnitTests.Messages
         [TestMethod]
         public void StartedAuthentication_Equals()
         {
-            StartedAuthentication sameStartedAuthentication =
-                new StartedAuthentication(TestConts.SERVER_CHALLENGE_SIGN_BASE64, TestConts.APP_ID_ENROLL,
-                                          TestConts.KEY_HANDLE_BASE64);
+            StartedAuthentication sameStartedAuthentication = new StartedAuthentication(
+                TestConts.SERVER_CHALLENGE_SIGN_BASE64, 
+                TestConts.APP_ID_ENROLL,
+                TestConts.KEY_HANDLE_BASE64);
             StartedAuthentication startedAuthentication = StartedAuthentication.FromJson(JsonData);
 
             Assert.IsTrue(startedAuthentication.Equals(sameStartedAuthentication));
