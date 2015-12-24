47a6c1bd46a264908116fe827994a46e97b2520c
Bryce Foster <bryce1337@gmail.com>
2015-12-24 09:18:02 -0600

    added unit test
    Added simple unit tests for random challenge generator.

---
+++ UnitTests/U2F/RandomChallengeGeneratorUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using u2flib.Crypto;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RandomChallengeGeneratorUnitTests
+    {
+        [TestMethod]
+        public void RandomChallengeGenerator_RandomChanlleges()
+        {
+            RandomChallengeGenerator randomChallengeGenerator = new RandomChallengeGenerator();
+            byte[] challenge1 = randomChallengeGenerator.GenerateChallenge();
+            byte[] challenge2 = randomChallengeGenerator.GenerateChallenge();
+
+            Assert.AreEqual(challenge1.Length, challenge2.Length);
+            Assert.IsFalse(challenge1.SequenceEqual(challenge2));
+        }
+    }
+}
