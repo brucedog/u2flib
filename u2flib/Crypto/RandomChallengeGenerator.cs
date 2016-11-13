df7d8895834fb31587a986a5c25a1243dfbcddd8
Bryce Foster <bryce1337@gmail.com>
2016-11-13 10:05:31 -0600

    Updated references
    updated the references such as bouncy castle crypto from 1.7 to 1.8

--- u2flib/Crypto/RandomChallengeGenerator.cs
+++ u2flib/Crypto/RandomChallengeGenerator.cs
@@ -6,19 +6,15 @@
  * license that can be found in the LICENSE file or at
  * https://developers.google.com/open-source/licenses/bsd
  */
-
-using Org.BouncyCastle.Crypto.Digests;
-using Org.BouncyCastle.Crypto.Prng;
+ 
 using Org.BouncyCastle.Security;
 
 namespace u2flib.Crypto
 {
     public class RandomChallengeGenerator : IChallengeGenerator
     {
-        // version 1.7 of bouncy castle library uses sha1 which the security industry seems to moving away from 
-        // so manual setting generator to sha256 which is the default in the upcoming 1.8 bouncy castle library.
-        private static readonly IRandomGenerator Generator = new DigestRandomGenerator(new Sha256Digest());
-        private static readonly SecureRandom Random = new SecureRandom(Generator);
+        // version 1.8.1 of BC uses sha256 be default
+        private static readonly SecureRandom Random = new SecureRandom();
 
         public byte[] GenerateChallenge()
         {
