1a509b70bcaf11aca7e8a583d5269e37848f6910
bryce foster <bryce1337@gmail.com>
2017-01-16 16:28:40 -0600

    Updated Refences
    Updated refences such as the aspnet site to MVC5.

--- DemoU2FSite/Bootstrapper.cs
+++ DemoU2FSite/Bootstrapper.cs
@@ -1,38 +1,42 @@
 using System.Web.Mvc;
 using BaseLibrary;
-using Microsoft.Practices.Unity;
-using Repositories;
-using Repositories.Context;
 using Services;
+using Repositories;
+using Microsoft.Practices.Unity;
 using Unity.Mvc4;
 
 namespace DemoU2FSite
 {
-    public static class Bootstrapper
+  public static class Bootstrapper
+  {
+    public static IUnityContainer Initialise()
     {
-        public static IUnityContainer Initialise()
-        {
-            var container = BuildUnityContainer();
+      var container = BuildUnityContainer();
 
-            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+      DependencyResolver.SetResolver(new UnityDependencyResolver(container));
 
-            return container;
-        }
+      return container;
+    }
 
-        private static IUnityContainer BuildUnityContainer()
-        {
-            var container = new UnityContainer();
-  
+      private static IUnityContainer BuildUnityContainer()
+      {
+          var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();    
+            container.RegisterType<IDataContext, Repositories.Context.DataContext>();
+            container.RegisterType<IMemeberShipService, MemeberShipService>();
+            container.RegisterType<IUserRepository, UserRepository>();
             RegisterTypes(container);
 
-            return container;
-        }
+          return container;
+      }
 
-        public static void RegisterTypes(IUnityContainer container)
-        {
-            container.RegisterType<IMemeberShipService, MemeberShipService>();
-            container.RegisterType<IDataContext, DataContext>();
-            container.RegisterType<IUserRepository, UserRepository>();
-        }
+      public static void RegisterTypes(IUnityContainer container)
+    {
+    
     }
+  }
 }
